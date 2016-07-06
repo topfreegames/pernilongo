a8856e24b69c4d6ac49c60062b201f0c47b89b2a
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-07-06 12:19:24 -0300

    initial commit with initial code structure

---
+++ index.js
@@ -0,0 +1,11 @@
+// pernilongo
+// https://github.com/topfreegames/pernilongo
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+module.exports = {
+  components: `${__dirname}/lib/components/`,
+  events: `${__dirname}/lib/events/`,
+}
