4b0592d3483d40747778efb8c94b516af3fd6c08
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-07-06 14:38:14 -0300

    add test to fix build

---
+++ tests/integration/common.js
@@ -0,0 +1,7 @@
+// pernilongo
+// https://github.com/topfreegames/pernilongo
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
