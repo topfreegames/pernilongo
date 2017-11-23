157dd3fa9e01c7d27778821228cf469017b9dc39
jlgm <jlgm@cin.ufpe.br>
2017-11-23 14:20:49 -0200

    update unit test for password generator

--- tests/unit/password.js
+++ tests/unit/password.js
@@ -4,10 +4,10 @@ var password = require('../../lib/password')
 
 describe('Password', function(){
 
-  it('should generate a valid password for using with mosquitto auth plugin', function(done){
+  it('should generate a valid password for using with the emqtt mongo auth plugin', function(done){
     var generatedPassword = password.getPBKDF2Hash('admin')
-    assert.equal(generatedPassword.length, 67)
-    expect(generatedPassword).to.match(/PBKDF2\$sha256\$901\$.{16}\$.{32}/)
+    assert.equal(generatedPassword.length, 76)
+    expect(generatedPassword).to.match(/PBKDF2\$sha256\$1000\$.{16}\$.{40}/)
     done()
   })
 
